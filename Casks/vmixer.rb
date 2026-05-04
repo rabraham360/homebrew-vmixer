ccbc6c125d528e789b5972eba2b7423204f2c1d2
rabraham360 <67745939+rabraham360@users.noreply.github.com>
2026-05-03 21:11:19 -0400

    Update vmixer.rb

--- Casks/vmixer.rb
+++ Casks/vmixer.rb
@@ -1,6 +1,6 @@
 cask "vmixer" do
   version "1.0.0"
-  sha256 "ca49821e055986f1ac4fb1f1c5a9b62c4bc88fe31a63d0a134d8c9e2b7aff3d8"
+  sha256 "00da7d7db44c13bc6f22fa115375fa10e7f9558057e38ea819e56c573696f0eb"
 
   url "https://github.com/rabraham360/vmixer/releases/download/v1.0.0/VMixer.zip"
   
