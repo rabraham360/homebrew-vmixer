89633f8715979432573dfaab0196f517f4f17fbe
rabraham360 <67745939+rabraham360@users.noreply.github.com>
2026-05-05 08:37:32 -0400

    Update vmixer.rb v1.1.0

--- Casks/vmixer.rb
+++ Casks/vmixer.rb
@@ -1,8 +1,8 @@
 cask "vmixer" do
-  version "1.0.0"
-  sha256 "00da7d7db44c13bc6f22fa115375fa10e7f9558057e38ea819e56c573696f0eb"
+  version "1.1.0"
+  sha256 "4350ab0b23ca24fb38dc5bf42e0d01f0d6febdbd105c5d19e9ace632c0b69370"
 
-  url "https://github.com/rabraham360/vmixer/releases/download/v1.0.0/VMixer.zip"
+  url "https://github.com/rabraham360/vmixer/releases/download/v1.1.0/VMixer.zip"
   
   name "VMixer"
   desc "Virtual volume mixer and audio router for macOS"
